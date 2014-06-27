e6ebd49144d7d2e6ad2dda395deec1b818ee62ca
Surendra Paluri <surendra.paluri@gmail.com>
2014-06-28 00:25:45 +0530

    Initial commit
    Initial commit

---
+++ src/com/amex/utils/Constants.java
@@ -0,0 +1,21 @@
+package com.amex.utils;
+
+public class Constants {
+
+public static final String URL 					   = "http://192.168.1.16:8080/EmployeeData";
+//public static final String URL 					   = "https://dwww420.app.aexp.com";
+public static final String REQUEST_PROP_PATH       = "\\src\\com\\amex\\config\\request\\Request.properties";
+public static final String ERROR_CODES_PROP_PATH   = "\\src\\com\\amex\\config\\errorhandles\\ErrorCodes.properties";
+public static final String SHEET_NAME              = "Sheet1";
+public static final String TESTDATA_PATH           = "\\src\\com\\amex\\testdata\\API_Details.xlsx";
+public static final String COLUMN_API              = "API";
+public static final String COLUMN_HEADER_KEY       = "Header_Key";
+public static final String COLUMN_HEADER_VALUE     = "Header_Value";
+public static final String COLUMN_REQUEST_NAME     = "Request_Name";
+public static final String COLUMN_REQUEST_PARAM    = "Request_Parameters";
+public static final String COLUMN_RESPONSE_CODE    = "Expected_Response_Code";
+public static final String CONTENT_TYPE_JSON       = "application/json";
+public static final Object COLUMN_REQUEST_METHOD   = "Request_Method";
+public static final Object COLUMN_ERROR_NAME       = "Error_Name";
+public static final Object COLUMN_RESPONSE_KEY     = "Response_Keys";
+}
