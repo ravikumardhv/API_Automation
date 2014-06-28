6e8942dd2c17739ebfc3f030bcd2115061c7a28c
sanjoecj <sanjoecj@gmail.com>
2014-06-28 23:19:57 +0530

    Added FileUtil

---
+++ src/com/amex/utils/FileUtil.java
@@ -0,0 +1,52 @@
+package com.amex.utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileUtil
+{
+	public static void createFile(String filepath, String fileName, String content) throws IOException
+	{
+		try{
+			
+		
+		if (createDirectory(filepath))
+		{
+			File file = new File(filepath + fileName);
+			// if file doesnt exists, then create it
+			file.createNewFile();
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+
+			System.out.println("Done");
+			System.out.println("Directory/File is created!");
+
+		}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static boolean createDirectory(String path)
+	{
+		File file = new File(path);
+		if (!file.exists())
+		{
+			if (file.mkdirs())
+			{
+				return true;
+			}
+		} else
+		{
+			return true;
+		}
+		return false;
+	}
+}
